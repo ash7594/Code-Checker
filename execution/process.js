236e67e8beba6d0d7f2bd663d4e659d8738649f2
Ashutosh Nath Agarwal <ash7594@gmail.com>
2016-03-17 21:55:07 +0530

    compiling edit

--- execution/process.js
+++ execution/process.js
@@ -2,21 +2,31 @@ var child_proc = require('child_process'),
 	fs = require('fs');
 
 function spawn (lang, code, customTest, cback) {
-	var proc, codefile = "./users/code.cpp", testfiles = [];
+	var proc, path = "./users/", codefile = "code.cpp", testfiles = [],
+		execfile = "a.out";
+
+	function messageHandler(m) {
+
+	}
 
 	function forkReady () {
 		proc = child_proc.fork('./execution/sandbox.js', [], { silent: true });
-		proc.send({ lang: lang, codefile: codefile, testfiles: testfiles });
+		proc.send({ lang: lang, execfile: execfile, testfiles: testfiles });
 	}
 
 	function compileReady() {
-		child_proc.exec('g++ ' + codefile, function (err, stdout, stderr) {
-			if (err) {
-				return cback(null);
-			}
-			if (stderr) cback(stderr);
-			else cback("success");
-		});
+		child_proc.exec(
+				'g++'.concat(' ', codefile, ' -o ', execfile),
+				{ cwd : path }, function (err, stdout, stderr) {
+					if (err) {
+						return cback(null);
+					}
+					if (stderr) {
+						return cback(stderr);
+					}
+					cback("success");
+					//setImmediate(forkReady);
+				});
 	}
 
 	function getTests() {
@@ -29,7 +39,7 @@ function spawn (lang, code, customTest, cback) {
 		});
 	}
 
-	fs.writeFile(codefile, code, function(err) {
+	fs.writeFile(path.concat(codefile), code, function(err) {
 		if (err) {
 			return cback(null);
 		}
--- users/code.cpp
+++ users/code.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 
 int main() {
+	cout<<"yoyo";
 	return 0;
 }
